6d3fc91d440d817e34c8455b8cd8aba43da246dd
Leonel Llane <llane4@hotmail.es>
2021-10-10 23:27:19 -0300

    game

---
+++ src/components/Board/Board.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Square from "../Square/Square";
+
+const style = {
+  border: "4px solid darkblue",
+  borderRadius: "10px",
+  width: "250px",
+  height: "250px",
+  margin: "0 auto",
+  display: "grid",
+  gridTemplate: "repeat(3, 1fr)/ repeat(3, 1fr)",
+};
+const Board = ({ squares, onClick }) => (
+  <div style={style}>
+    {squares.map((square, i) => {
+      return <Square value={square} key={i} onClick={() => onClick(i)} />;
+    })}
+  </div>
+);
+
+export default Board;
